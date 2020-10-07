d1949f16eb3af68882de9573dc6d540d0bbc77a5
joeljkbs <joeljacksebastian@gmail.com>
2020-10-06 21:35:14 -0400

    Basic Template

---
+++ app/src/main/java/com/example/cst8334_glutentracker/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.cst8334_glutentracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/example/cst8334_glutentracker/scanActivity.java
@@ -0,0 +1,14 @@
+package com.example.cst8334_glutentracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class scanActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scan);
+    }
+}
