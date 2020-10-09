ad40cc730be5702e05276e9813913114135bcf35
Naimul Rahman <rahm0075@algonquinlive.com>
2020-10-08 21:16:06 -0400

    Added code to cartActivity.

--- app/src/main/java/com/example/cst8334_glutentracker/MainActivity.java
+++ app/src/main/java/com/example/cst8334_glutentracker/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.cst8334_glutentracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 
 //version feng
@@ -13,5 +16,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button toCart = findViewById(R.id.toCart);
+        toCart.setOnClickListener((v) ->{startActivity(new Intent(MainActivity.this, cartActivity.class));});
     }
 }
