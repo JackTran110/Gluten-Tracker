8e2eacf418cb1f48ca47a3686e371dbe8aa36b83
sun00086 <71674259+sun00086@users.noreply.github.com>
2020-10-13 08:35:11 -0400

    feng,added sample data for the list

--- app/src/main/java/com/example/cst8334_glutentracker/ReportActivity.java
+++ app/src/main/java/com/example/cst8334_glutentracker/ReportActivity.java
@@ -3,6 +3,9 @@ package com.example.cst8334_glutentracker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 
 public class ReportActivity extends AppCompatActivity {
 
@@ -10,5 +13,11 @@ public class ReportActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_report);
+
+        String[] strLst = {"Rec.Id   Date   Item    SubTotal   Taxdec.","No.563   Oatmeal    $56.23   $1.56","No.003    Pasta   $34.23   $0.86","No.897   Cereals    $126.50   $5.06"};
+        ListAdapter theAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,strLst);
+        ListView theListView = (ListView) findViewById(R.id.rptlst);
+        theListView.setAdapter(theAdapter);
+
     }
 }
