646fc64800191c2a1e77490a8ae8247dce65153e
sun00086 <71674259+sun00086@users.noreply.github.com>
2020-10-12 23:12:27 -0400

    feng,the layout of the report

--- app/src/main/java/com/example/cst8334_glutentracker/ReportActivity.java
+++ app/src/main/java/com/example/cst8334_glutentracker/ReportActivity.java
@@ -1,6 +1,14 @@
 package com.example.cst8334_glutentracker;
 
-public class ReportActivity {
+import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Bundle;
 
+public class ReportActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_report);
+    }
 }
