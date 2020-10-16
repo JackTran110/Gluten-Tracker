27e84076da22790d17bfa2eb54fe004022a8c78f
lama0088 <lama0088@algonquinlive.com>
2020-10-16 11:58:55 -0400

    Alex Barcode Scanner class

--- app/src/main/java/com/example/cst8334_glutentracker/ScanActivity.java
+++ app/src/main/java/com/example/cst8334_glutentracker/ScanActivity.java
@@ -3,8 +3,14 @@ package com.example.cst8334_glutentracker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.SurfaceView;
+import android.widget.Button;
 
 public class ScanActivity extends AppCompatActivity {
+
+    Button okScanButton = (Button) findViewById(R.id.okEditTextButton);
+    Button cancelScanButton = (Button) findViewById(R.id.cancelEditTextButton);
+    SurfaceView cameraView = (SurfaceView) findViewById(R.id.scanSurfaceView);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
